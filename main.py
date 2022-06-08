2e0c3038aa8dfc9e6f1f286c6c3be60a8bbc37e7
Lindsey Musel <lmusel@godaddy.com>
2022-06-08 13:56:04 -0500

    Add elasticAPM and kustomization

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import logging
 import os
 
+from elasticapm import Client, instrument
 from pika.exceptions import ChannelWrongStateError
 
 from rabbitmq.consumer import Consumer
@@ -12,12 +13,19 @@ url = multi_url
 
 env = os.getenv('sysenv')
 
+instrument()
+apm = Client(service_name='requeue', env=env)
+
+
 if __name__ == '__main__':
     p = Publisher(url, env)
     c = Consumer(url, p, env)
     while True:
+        apm.begin_transaction("start queuing")
         try:
             c.consume()
+            apm.end_transaction("consume queue", "success")
         except ChannelWrongStateError as cc:
             logging.debug(f'Channel has closed stopping consumer: {cc}')
+            apm.end_transaction("end queueing", "failure")
             exit(0)
